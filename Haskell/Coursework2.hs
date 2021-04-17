86deed45385347535888504101c340dbf4f05e12
Wyson <wyson002@gmail.com>
2021-04-17 15:25:50 +0800

    update

--- Haskell/Coursework2.hs
+++ Haskell/Coursework2.hs
@@ -1,1 +1,37 @@
 chomp :: String -> String
+chomp str = takeWhile (== head str) str
+
+munch :: String -> String
+munch = take 9 . chomp
+
+runs :: String -> [String]
+runs "" = []
+runs str = extract str [] where
+    extract str xs
+        |   length str == 0 = xs
+        |   otherwise = extract (drop (length (munch str)) str) (xs ++ [(munch str)])
+
+encode :: String -> [(Char,Int)]
+encode [] = []
+encode str  = ((x !! 0),(length x)):(encode (drop (length x) (str)))
+    where (x:xs) = runs str
+
+flatten :: [(Char,Int)] -> String
+flatten [] = []
+flatten (x:xs) = (fst x):(show (snd x) ++ flatten xs)
+
+compress [] = []
+compress str = flatten (encode str)
+
+decode :: [(Char,Int)] -> String
+decode [] = []
+decode (x:xs) = ((take (snd x) (repeat (fst x)))) ++ decode xs
+
+expand :: String -> [(Char,Int)]
+expand [] = []
+expand [x] = []
+expand (x:y:xs) = (x,(fromEnum y - fromEnum '0')):(expand xs)
+
+decompress :: String -> String
+decompress [] = []
+decompress str = decode (expand str)
