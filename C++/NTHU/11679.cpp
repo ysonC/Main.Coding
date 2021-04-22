e776a77c817d65a1df22e3e7bd0e167e407fd31b
Wyson <wyson002@gmail.com>
2021-04-22 17:56:39 +0800

    aadd new files

--- C++/NTHU/11679.cpp
+++ C++/NTHU/11679.cpp
@@ -2,20 +2,126 @@
 #include<math.h>
 using namespace std;
 
-//4 2 6 1 3 5 7 
-//1 2 3 4 5 6 7
+//4  2 6  1 3 5 7 
+//1  2 3  4 5 6 7
 
-//8 4 12 2 6 10 14 1 3 5 7 9 11 13 15
+//8     4 12     2 6 10 14      1 3 5 7 9 11 13 15
 //1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 
 //0 1 2 3 4 5 6
-// 
+//
+
 int main()
 {
-    string input;
+    char input[100];
+    char stored[100];
     int length, lvl;
-    cin >> length;
-    cin >> input;
-    lvl = (length+1)%2;
 
+    cin >> length;
+    int mid = (length-1) / 2;
+    cin >> input;
+
+    lvl = log2(length+1) -1;
+
+//2^3 2^2  2^1
+//4  2 6  1 3 5 7 
+//1  2 3  4 5 6 7
+    int number;
+    int arrayN = 1;
+    int cnt = 2;
+
+    /**
+    for (int i = 1; i <= pow(2,lvl); i*=2) // j = 1, 2, 4
+    {
+        number = pow(2,lvl);
+        cout << number << endl;
+        for (int j = 1; j > i; j++) // j = 1,2,3,4 
+        {
+            stored[arrayN] = input[number];
+            cout << arrayN << ""; 
+            arrayN += cnt;
+            number++;
+        }
+        arrayN = 0;
+        arrayN += cnt;
+        cnt++;
+
+        number /= 2;
+    }
+    **/
+
+    for (int i = pow(2,lvl); lvl >= 0; i/=2) // i = 4 , 2, 1
+    {
+        //cout << "i = " << i << endl;
+        //cout << "lvl = " << lvl << endl;
+        number = i - 1;
+        for (int j = 1; j > i; j++) // j = 1,2,3,4 
+        {
+            stored[arrayN] = input[number];
+            cout << arrayN << ""; 
+            arrayN += cnt;
+            number++;
+        }
+        arrayN = 0;
+        arrayN += cnt;
+        cnt++;
+
+        lvl--;
+    }
+
+    cout << "\n";
+    cout << stored << endl;
 }
+
+// int func(int a, int b, int c1 , int c2)
+// {
+//     char redo[10];
+//     char dothis[10] = {4,2,6,1,3,5,7};
+//     int mid = 3;
+//     int lvl = 2;
+//     for (int i = 0; i < 100; i++)
+//     {
+//         if (i == 0)
+//             redo[mid - lvl] = dothis[i];
+        
+//     }
+// }
+// int main()
+// {
+//     string input;
+//     string stored;
+//     int length, lvl;
+//     int a,b;
+
+//     cin >> length;
+//     int mid = (length-1) / 2;
+//     cin >> input;
+
+//     lvl = log2(length+1) -1;
+//     cout << "lvl = " << lvl << endl;
+//     cout << "mid = " << mid << endl;
+
+//     // stored[0] = input[mid];
+
+//     int j = 0;
+
+
+
+//     for (int i = 1; i < input.length(); i = pow(2,i))
+//     {
+//         for (int j; i <= i; j ++)
+//         {
+//             if (j == 0)
+//                 stored[j] = input[mid];
+//             else if (j == 1)
+//                 stored[j] = input[mid -= lvl];
+//             else if (j % 2 == 0)
+//                 stored[j] = input[mid += lvl];
+//             else
+//                 stored[j] = input[mid -= lvl];
+//         }
+//         lvl--;
+//     }
+
+//     cout << stored << endl;
+//}
---
+++ WebStuff/add_stuff.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+        <title>Blockbuster Database</title>
+        <LINK REL='stylesheet' TYPE='text/css' HREF='dbicw.css'>
+</head>
+<body>
+<h1>Movie Table</h1>
+
+<?php
+$addMovie = $_GET['newMovie']; // change this
+$addPrice = $_GET['newPrice'];
+$addYear = $_GET['newYear'];
+$addGenre = $_GET['newGenre'];
+$addActor = $_GET['newActor'];
+
+$db_host = 'mysql.cs.nott.ac.uk';
+$db_user = 'efyhc5_COMP1004'; // change me
+$db_pass = '000222isa'; // change me
+$db_name = 'efyhc5_COMP1004'; // change me
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($conn->connect_errno)  die("failed to connect to database\n</body>\n</html>"); 
+
+$conn->query("INSERT into Actor (actID, actName) VALUES (0,'$addActor');");
+$conn->query("INSERT into Movie (actID, mvTitle, mvPrice, mvYear, mvGenre) 
+                Values 
+                    ((SELECT actID from Actor where actName = '$addActor'), '$addMovie', $addPrice, $addYear, '$addGenre');");
+
+$sql = "SELECT mvID, mvTitle, mvPrice, mvYear, mvGenre FROM Movie";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$stmt->bind_result($ID, $Title, $Price, $Year, $Genre );
+?>
+
+<table width="750" border="1" cellpadding="1" cellspacing="1">
+<tr> <th>ID</th> <th>Title</th> <th>Price</th> <th>Year</th> <th>Genre</th> </tr>
+
+<?php
+while($stmt->fetch()){
+  echo "<tr>";
+  echo "<td>". htmlentities($ID) ."</td>";
+  echo "<td>". htmlentities($Title) ."</td>";
+  echo "<td>". htmlentities($Price) ."</td>";
+  echo "<td>". htmlentities($Year) ."</td>";
+  echo "<td>". htmlentities($Genre) ."</td>";
+  echo "</tr>";
+}
+?>
+</table>
+</body>
+</html>
