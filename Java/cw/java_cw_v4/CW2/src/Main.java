e906f6e9cdcaffda95eac7358644cb7fe4a9e1a9
Huai-Sheng Cheng <efyhc5@nottingham.ac.uk>
2021-05-21 21:38:33 +0800

    backup

--- Java/cw/java_cw_v4/CW2/src/Main.java
+++ Java/cw/java_cw_v4/CW2/src/Main.java
@@ -1,13 +1,16 @@
 import java.awt.Color;
 import java.util.Random;
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 //import java.awt.event.ActionEvent;
 //import java.awt.event.ActionListener;
+import javax.swing.Timer;
 
 public class Main extends JFrame implements ActionListener
 {
@@ -20,6 +23,7 @@ public class Main extends JFrame implements ActionListener
 
     int flag = 1; // start from white
     int white = 1 , black = 0; 
+    int time_s=0;
     Pieces moves[][] = new Pieces[100][100];
 
     public void createGUI()
@@ -75,7 +79,7 @@ public class Main extends JFrame implements ActionListener
                 {
                     moves[i][j] = new Pieces();
                     moves[i][j].setColor(Color.GREEN);
-                    moves[i][j].setBackground(Color.GREEN);
+                    //moves[i][j].setBackground(Color.GREEN);
                     moves[i][j].setPreferredSize(new Dimension(60,60));
                     moves[i][j].storeArray(i, j);
                     gridPanel.add(moves[i][j]);
@@ -87,16 +91,31 @@ public class Main extends JFrame implements ActionListener
         guiFrame.repaint();
         guiFrame.pack(); // Resize frame to fit content
         guiFrame.setVisible(true);
+        
+        Time time = new Time();
     }
-    
- 
+    public class Time extends JFrame implements ActionListener 
+    {
+        
+        public Time()
+        {
+            new Timer(1000,this).start();
+            while(true);
+        }
+     
+        @Override
+        public void actionPerformed(ActionEvent e) 
+        {
+            time_s++;
+        }
+    }
 
     public class Pieces extends JButton implements ActionListener 
     {
         boolean lock = false;
         Color color;
-        int row, column;
         int placeAble;
+        int row, column;
         
         public Pieces()
         {
@@ -191,6 +210,15 @@ public class Main extends JFrame implements ActionListener
             int totalWhite = 0, totalBlack = 0;
             
             System.out.println("White : " + getTotalWhite(totalWhite) + " Black : " + getTotalBlack(totalBlack));
+
+            if(getTotalWhite(totalWhite) +  getTotalBlack(totalBlack) == 64)
+            {
+                if (getTotalWhite(totalWhite) >  getTotalBlack(totalBlack)) 
+                {
+                    JOptionPane.showMessageDialog(guiFrame, "White wins!!" + "Time : " + time_s);
+                }               
+                else JOptionPane.showMessageDialog(guiFrame, "Black wins!!"+ "Time : " + time_s);
+            }
         }
 
 
@@ -296,7 +324,7 @@ public class Main extends JFrame implements ActionListener
                 {
                     moves[row][i-1].setColor(this.getColor());
                     addPlaceAble();
-                    System.out.println("left to right");
+                    //System.out.println("left to right");
                     findSameColor1();
                 }
             }
@@ -312,7 +340,7 @@ public class Main extends JFrame implements ActionListener
                 {
                     moves[row][i+1].setColor(this.getColor());
                     addPlaceAble();
-                    System.out.println("right to left");
+                    //System.out.println("right to left");
                     findSameColor2();
                 }   
             }
@@ -328,7 +356,7 @@ public class Main extends JFrame implements ActionListener
                 {
                     moves[i+1][column].setColor(this.getColor());
                     addPlaceAble();
-                    System.out.println("down to up");  
+                    //System.out.println("down to up");  
                     findSameColor3(); 
                 }
             }
@@ -344,7 +372,7 @@ public class Main extends JFrame implements ActionListener
                 {
                     moves[i-1][column].setColor(this.getColor());
                     addPlaceAble();
-                    System.out.println("up to down");   
+                    //System.out.println("up to down");   
                     findSameColor4();
                 }
             }
@@ -446,13 +474,271 @@ public class Main extends JFrame implements ActionListener
             {
                 makeMove();
                 combineFind();
+                // int[] cross = countCross();
+                // //System.out.println("up : " + cross[0] + " down : " + cross[1] + " left : " + cross[2]+" right : " + cross[3]);
+                // int[] X = countX();
+                // //System.out.println("downLeft : " + X[0] + " downRight : " + X[1] + " upLeft : " + X[2]+" upRight : " + X[3]);
+                // System.out.println(X[2] +" "+ cross[0]+" " + X[3]);
+                // System.out.println(cross[2] +" "+ " " +" "+ cross[3]);
+                // System.out.println(X[0] +" "+ cross[1] +" "+ X[1]);
+                
                 if(getPlaceAble() == 0) reverseMove();
             }
+            
             System.out.println("array : " + getArray() 
-                                + " lock : " + getLock() + " placeAble : " + getPlaceAble());
+            + " lock : " + getLock() + " placeAble : " + getPlaceAble());
+            System.out.println("flag = " + flag);
             packGetTotal();
+            System.out.println("------------------------------");
+        }
+        
+        public int[] countCross()
+        {
+            Color tmp = null;
+            Color tmp2 = null;
+            if (flag == white) tmp = Color.BLACK;
+            else tmp2 = Color.WHITE;
+            int[] ans = new int[4];
+            int upCnt = 0;
+            
+            int final_up =0;         
+            int final_down = 0;
+            int final_left = 0;
+            int final_right = 0;   
+            
+            int i = 1;
+            while(moves[row-i][column].getColor() == tmp)
+            {   
+                System.out.println("up is here");
+                if(moves[row-i][column].getColor() == Color.GREEN) 
+                {
+                    upCnt = 0;
+                    break;
+                }
+                else if (moves[row-i][column].getColor() == tmp2)
+                {
+                    final_up = upCnt;
+                    break;
+                }
+                upCnt++;
+                i++;
+            }
+            upCnt--;
+            if(upCnt < 0) upCnt = 0;
+    
+            i = 1;
+            int downCnt = 0;
+            while(moves[row+i][column].getColor() == tmp)
+            {
+                if(moves[row+i][column].getColor() == Color.GREEN) 
+                {
+                    downCnt = 0;
+                    break;
+                }
+                else if (moves[row+i][column].getColor() == tmp2)
+                {
+                    final_down = downCnt;
+                    break;
+                }
+                downCnt++;
+                i++;
+            }
+            downCnt--;
+            if(downCnt < 0) downCnt = 0;
+
+            i = 1;
+            int leftCnt = 0;
+            while(moves[row][column-i].getColor() == tmp)
+            {
+                if(moves[row][column-i].getColor() == Color.GREEN) 
+                {
+                    leftCnt = 0;
+                    break;
+                }
+                else if (moves[row][column-i].getColor() == tmp2)
+                {
+                    final_left = leftCnt;
+                    break;
+                }
+                leftCnt++;
+                i++;
+            }
+            leftCnt--;
+            if(leftCnt < 0) leftCnt = 0;
+
+            i = 1;
+            int rightCnt = 0;
+            while(moves[row][column+i].getColor() == tmp)
+            {
+                if(moves[row][column+i].getColor() == Color.GREEN) 
+                {
+                    rightCnt = 0;
+                    break;
+                }
+                else if (moves[row][column+i].getColor() == tmp2)
+                {
+                    final_right = rightCnt;
+                    break;
+                }
+                rightCnt++;
+                i++;
+            }
+            rightCnt--;
+            if(rightCnt < 0) rightCnt = 0;
+
+
+            ans[0] = final_up;
+            ans[1] = final_down;
+            ans[2] = final_left;
+            ans[3] = final_right;
+            return ans;
         }
 
+
+        public int[] countX()
+        {
+            int i = 1;
+            Color tmp = null;
+            if (flag == white) tmp = Color.BLACK;
+            else tmp = Color.WHITE;
+            int[] ans = new int[4];
+            int downLeftCnt = 0;
+            int downRightCnt = 0;
+            int upLeftCnt = 0;
+            int upRightCnt = 0;
+            
+            int final_dLC = 0;
+            int final_dRC = 0;
+            int final_uLC = 0;
+            int final_uRC = 0;
+            
+
+            while(moves[row+i][column-i].getColor() == tmp)
+            {
+                if(moves[row+i][column-i].getColor() == Color.GREEN) 
+                {
+                    downLeftCnt = 0;
+                    break;
+                }
+                else if (moves[row+i][column-i].getColor() == getColor())
+                {
+                    final_dLC = downLeftCnt;
+                    break;
+                }
+                    
+                downLeftCnt++;
+                i++;
+            }
+            downLeftCnt--;
+            if(downLeftCnt < 0) downLeftCnt = 0;
+
+            i = 1;
+            while(moves[row+i][column+i].getColor() == tmp)
+            {
+                if(moves[row+i][column+i].getColor() == Color.GREEN) 
+                {
+                    downRightCnt = 0;
+                    break;
+                }
+                else if (moves[row+i][column+i].getColor() == getColor())
+                {
+                    final_dRC = downRightCnt;
+                    break;
+                }
+                downRightCnt++;
+                i++;
+            }
+            downRightCnt--;
+
+            if(downRightCnt < 0) downRightCnt = 0;
+
+            i = 1;
+            while(moves[row-i][column-i].getColor() == tmp)
+            {
+                if(moves[row-i][column-i].getColor() == Color.GREEN) 
+                {
+                    upLeftCnt = 0;
+                    break;
+                }
+                else if (moves[row-i][column-i].getColor() == getColor())
+                {
+                    final_uLC = upLeftCnt;
+                    break;
+                }
+                upLeftCnt++;
+                i++;
+            }
+            upLeftCnt--;
+            if(upLeftCnt < 0) upLeftCnt = 0;
+
+            i = 1;
+            while(moves[row-i][column+i].getColor() == tmp)
+            {
+                if(moves[row-i][column+i].getColor() == Color.GREEN) 
+                {
+                    upRightCnt = 0;
+                    break;
+                }
+                else if (moves[row+i][column-i].getColor() == getColor())
+                {
+                    final_uRC = upRightCnt;
+                    break;
+                }
+                upRightCnt++;
+                i++;
+            }
+            upRightCnt--;
+            if(upRightCnt < 0) upRightCnt = 0;
+            
+            ans[0] = final_dLC;
+            ans[1] = final_dRC;
+            ans[2] = final_uLC;
+            ans[3] = final_uRC;
+            return ans;
+        }
+
+        public int[] calcBestMove()
+        {
+            int[] bestArray = new int[2];
+            int bestRow = 1, bestColumn = 1;
+            int i = 100;
+            int compareA = 0;
+            int compareB = 0;
+
+            int rRow = rand.nextInt(8)+1;
+            int rColumn = rand.nextInt(8)+1;
+            while(i>0)
+            {
+                while(moves[rRow][rColumn].ifPass() == false && moves[rRow][rColumn].getPlaceAble() > 0)
+                {
+                    rRow = rand.nextInt(8)+1;
+                    rColumn = rand.nextInt(8)+1;
+                }
+                //System.out.println(rRow + " ||" + rColumn);
+                int[] crossTmp = moves[rRow][rColumn].countCross();
+                int[] XTmp = moves[rRow][rColumn].countX();
+                compareA = crossTmp[0] + crossTmp[1] + crossTmp[2] + crossTmp[3] + XTmp[0] + XTmp[1] + XTmp[2] + XTmp[3];
+                if (compareA > compareB)
+                {
+                    bestRow = rRow;
+                    bestColumn = rColumn;
+                    compareB = compareA;
+                    //System.out.println("BEST : " + bestRow + " , " + bestColumn);
+                }
+                else
+                {
+                    rRow = rand.nextInt(8)+1;
+                    rColumn = rand.nextInt(8)+1;
+                }
+                i--;
+            }
+            System.out.println(compareB);
+            System.out.println("!!!!" + bestRow + " , " + bestColumn);
+            bestArray[0] = bestRow;
+            bestArray[1] = bestColumn;
+            return bestArray;
+        }
+        
     }
 
     @Override
@@ -478,8 +764,8 @@ public class Main extends JFrame implements ActionListener
                 else
                 {
                     pass = true;
+                    System.out.println(moves[i][j].getArray());
                 }
-                System.out.println(moves[i][j].getArray());
             }
             else
             {
